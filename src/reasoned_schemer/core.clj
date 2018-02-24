1001ca239b4ebde6ddc1ead415408cc369a5d89e
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-02-24 16:55:45 -0500

    Finish chapter 4

--- src/reasoned_schemer/core.clj
+++ src/reasoned_schemer/core.clj
@@ -1029,3 +1029,187 @@ u#
 (run* [x]
   (memberrevo x '(pasta e fagioli)))
 ;; => (pasta e fagioli)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 4. Members Only
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn mem
+  [x l]
+  (cond
+   (empty? l) false
+   (eq-first? l x) l
+   :else (mem x (rest l))))
+
+(mem 'tofu '(a b tofu d peas e))
+;; => (tofu d peas e)
+
+(mem 'tofu '(a b peas d peas e))
+;; => false
+
+(run* [out]
+  (== (mem 'tofu '(a b tofu d peas e)) out))
+;; => ((tofu d peas e))
+
+(mem 'peas
+     (mem 'tofu '(a b tofu d peas e)))
+;; => (peas e)
+
+(mem 'tofu
+     (mem 'tofu '(a b tofu d tofu e)))
+;; => (tofu d tofu e)
+
+(mem 'tofu
+     (rest (mem 'tofu '(a b tofu d tofu e))))
+;; => (tofu e)
+
+(defn memo
+  [x l out]
+  (conde
+   [(eq-firsto l x) (== l out)]
+   [s# (fresh [d]
+         (resto l d)
+         (memo x d out))]))
+
+(run 1 [out]
+  (memo 'tofu '(a b tofu d tofu e) out))
+;; => ((tofu d tofu e))
+
+(run 1 [out]
+  (fresh [x]
+    (memo 'tofu (list 'a 'b x 'd 'tofu 'e) out)))
+;; => ((tofu d tofu e))
+
+(run* [r]
+  (memo r
+        '(a b tofu d tofu e)
+        '(tofu d tofu e)))
+;; => (tofu)
+
+(run* [q]
+  (memo 'tofu '(tofu e) '(tofu e))
+  (== true q))
+;; => (true)
+
+(run* [q]
+  (memo 'tofu '(tofu e) '(tofu))
+  (== true q))
+;; => ()
+
+(run* [x]
+  (memo 'tofu '(tofu e) (list x 'e)))
+;; => (tofu)
+
+(run* [x]
+  (memo 'tofu '(tofu e) (list 'peas x)))
+;; => ()
+
+(run* [out]
+  (fresh [x]
+    (memo 'tofu (list 'a 'b x 'd 'tofu 'e) out)))
+;; => ((tofu d tofu e) (tofu e))
+
+(run 12 [z]
+  (fresh [u]
+    (memo 'tofu (llist 'a 'b 'tofu 'd 'tofu 'e z) u)))
+;; => (_0
+;;     _0
+;;     (tofu . _0)
+;;     (_0 tofu . _1)
+;;     (_0 _1 tofu . _2)
+;;     (_0 _1 _2 tofu . _3)
+;;     (_0 _1 _2 _3 tofu . _4)
+;;     (_0 _1 _2 _3 _4 tofu . _5)
+;;     (_0 _1 _2 _3 _4 _5 tofu . _6)
+;;     (_0 _1 _2 _3 _4 _5 _6 tofu . _7)
+;;     (_0 _1 _2 _3 _4 _5 _6 _7 tofu . _8)
+;;     (_0 _1 _2 _3 _4 _5 _6 _7 _8 tofu . _9))
+
+(defn rember
+  [x l]
+  (cond
+    (empty? l) '()
+    (eq-first? l x) (rest l)
+    :else (cons (first l)
+                (rember x (rest l)))))
+
+(rember 'peas '(a b peas d peas e))
+;; => (a b d peas e)
+
+(rember 'peas
+        (rember 'peas '(a b peas d peas e)))
+;; => (a b d e)
+
+(defn rembero
+  [x l out]
+  (conde
+   [(emptyo l) (== out '())]
+   [(eq-firsto l x) (resto l out)]
+   [s# (fresh [a d res]
+         (conso a d l)
+         (rembero x d res)
+         (conso a res out))]))
+
+(run 1 [out]
+  (fresh [y]
+    (rembero 'peas (list 'a 'b y 'd 'peas 'e) out)))
+;; => ((a b d peas e))
+
+(run* [out]
+  (fresh [y z]
+    (rembero y (list 'a 'b y 'd z 'e) out)))
+;; => ((b a d _0 e)
+;;     (a b d _0 e)
+;;     (a b d _0 e)
+;;     (a b d _0 e)
+;;     (a b _0 d e)
+;;     (a b e d _0)
+;;     (a b _0 d _1 e))
+
+(run* [r]
+  (fresh [y z]
+    (rembero y (list y 'd z 'e) (list y 'd 'e))
+    (== (list y z) r)))
+;; => ((d d)
+;;     (d d)
+;;     (_0 _0)
+;;     (e e))
+
+(rember 'd '(d d d e))
+;; => (d d e)
+
+(rember 'a '(a d a e))
+;; => (d a e)
+
+(rember 'e '(e d e e))
+;; => (d e e)
+
+;; wtf...
+;; these values are producing non-solutions to `rembero`...
+;; skipping to `surpriseo` out of confusion...
+
+(defn surpriseo
+  [s]
+  (rembero s '(a b c) '(a b c)))
+
+(run* [r]
+  (== 'd r)
+  (surpriseo r))
+;; => (d)
+
+(run* [r]
+  (surpriseo r))
+;; => (_0)
+
+(run* [r]
+  (surpriseo r)
+  (== r 'b))
+;; => (b)
+
+;; Seriously tho...dafuq is going on here
+(run* [r]
+  (== r 'b)
+  (surpriseo r))
+;; => (b)
